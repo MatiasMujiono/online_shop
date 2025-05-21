ccb89d3eb9b31599a8264e8c46158b89c4d234b3
MatiasMujiono <matiasmujiono6@gmail.com>
2025-05-21 14:31:48 +0700

    add flight's table

---
+++ database/migrations/2025_05_21_071917_create_flights_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('airline');
+            $table->date('time_start');
+            $table->date('time_end');
+            $table->text('description');
+            $table->date('delay_to')->nullable();
+            $table->string('destination');
+            $table->string('from');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flights');
+    }
+};
